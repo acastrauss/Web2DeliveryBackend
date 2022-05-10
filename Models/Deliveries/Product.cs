4e9e1320de2b7179f3ecf5f64461211177dcb48b
Aleksandar Stamenković <stamenkovic.pr34.2018@uns.ac.rs>
2022-05-10 03:10:51 +0200

    models created

---
+++ Models/Deliveries/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Deliveries
+{
+    // Inherit if specific products are needed
+    public class Product
+    {
+        public Product(string name, float pricePerOne, uint quantity)
+        {
+            Name = name;
+            PricePerOne = pricePerOne;
+            Quantity = quantity;
+        }
+
+        public Product(Product rhs)
+        {
+            Name = rhs.Name;
+            PricePerOne = rhs.PricePerOne;
+            Quantity = rhs.Quantity;
+        }
+
+        public String Name { get; set; }
+        public float PricePerOne { get; set; }
+        public uint Quantity { get; set; }
+
+        public bool IsValid()
+        {
+            return !String.IsNullOrEmpty(Name) && !String.IsNullOrWhiteSpace(Name) 
+                && Quantity != 0 && PricePerOne != 0;
+        }
+    }
+}
---
+++ Models/Users/Deliverer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Users
+{
+    public class Deliverer : User
+    {
+        public List<Deliveries.Delivery> NewDeliveries { get; set; }
+        public List<Deliveries.Delivery> MyDeliveries { get; set; }
+        public Deliveries.Delivery CurrentDelivery { get; set; }
+
+        public bool Verified { get; set; }
+
+        public Deliverer(Deliverer rhs) : base(rhs)
+        {
+            NewDeliveries = rhs.NewDeliveries;
+            MyDeliveries = rhs.MyDeliveries;
+            CurrentDelivery = rhs.CurrentDelivery;
+            Verified = rhs.Verified;
+        }
+
+        public Deliverer(
+            string username, string email, string password,
+            string firstName, string lastName, DateTime dateOfBirth,
+            string address, string picturePath, bool verified = false,
+            List<Deliveries.Delivery> myDeliveries = null,
+            List<Deliveries.Delivery> newDeliveries = null,
+            Deliveries.Delivery currentDelivery = null
+            ) :
+            base(
+                username, email, password, firstName,
+                lastName, dateOfBirth, address, picturePath, UserType.DELIVERER)
+        {
+            Verified = verified;
+
+            if(myDeliveries == null)
+            {
+                myDeliveries = new List<Deliveries.Delivery>();
+            }
+
+            if(newDeliveries == null)
+            {
+                newDeliveries = new List<Deliveries.Delivery>();
+            }
+
+            NewDeliveries = newDeliveries;
+            MyDeliveries = myDeliveries;
+            CurrentDelivery = currentDelivery;
+        }
+    }
+}
