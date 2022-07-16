2caeded56e171291d01ce6fba8b856c0a919eed9
Aleksandar Stamenković <stamenkovic.pr34.2018@uns.ac.rs>
2022-07-16 10:47:27 +0200

    showing picture

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -1,16 +1,20 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +39,12 @@ namespace Backend
 
             services.Configure<AppSettings>(Configuration.GetSection("ApplicationSettings"));
 
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
 
             services.AddCors(options =>
             {
@@ -91,6 +101,13 @@ namespace Backend
             app.UseHttpsRedirection();
 
             app.UseCors(_CorsName);
+
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"imgs")),
+                RequestPath = new PathString("/imgs")
+            });
 
             app.UseRouting();
 
